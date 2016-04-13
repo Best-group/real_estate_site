1aaac851cd9039f11183ec67a4536956b64ac606
Rebecca <beckylum0214@gmail.com>
2016-04-13 11:22:21 +0800

    add Index page
    Added Index Page

--- BuyMyCastle/test/controllers/index_controller_test.rb
+++ BuyMyCastle/test/controllers/index_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IndexControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get index_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get index_index_url
+    assert_response :success
+  end
+
 end
