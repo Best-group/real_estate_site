0a7ddebab7fd386ef12f9d04dec28d5df194b340
Rebecca <beckylum0214@gmail.com>
2016-04-13 14:47:07 +0800

    Added User Migration

---
+++ db/migrate/20160413060440_create_users.rb
@@ -0,0 +1,26 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :email
+      t.integer :subscriptionID
+      t.integer :phone
+      t.string :first_name
+      t.string :last_name
+      t.date :register_date
+      t.boolean :show_name
+      t.boolean :show_email
+      t.boolean :show_phone
+
+
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+
+end
