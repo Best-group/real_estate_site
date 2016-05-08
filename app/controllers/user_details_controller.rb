1e483fe7547f1fd3ebf564ca0247336ee61624a1
BrodieRickert <rickert.brodie@gmail.com>
2016-05-08 16:50:13 +0800

    updated user details
    now saves to DB

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,22 +1,42 @@
 class UserDetailsController < ApplicationController
   def create
+	
+	@user_detail = UserDetail.new(user_params)
+	
+	if @user_detail.save
+		puts "succse"
+	else
+		puts "fail"
+		end 
+		
   end
+  
 
   def destroy
   end
 
   def edit
+  
   end
 
   def index
   end
 
   def new
+	@user_detail = UserDetail.new
   end
 
   def show
+	#@user_details = user_details.find(params[id])
   end
 
   def update
   end
+ 
+ private 
+ 
+  def user_params
+  
+	params.require(:user_detail).permit(:name)
+end
 end
